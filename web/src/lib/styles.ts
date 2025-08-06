26ff9efa9da3d29fa7733c7c04f6c52431b57fbe
Ole Magnus Fon Johnsen <me@omfj.no>
2025-08-06 14:12:13 +0200

    Add color variables

--- web/src/lib/styles.ts
+++ web/src/lib/styles.ts
@@ -10,9 +10,9 @@ export function buttonStyles(
 	className?: ClassValue
 ) {
 	return cn(
-		'flex h-10 items-center justify-center gap-2 rounded-lg border-3 px-4 py-2 font-semibold ring-offset-2 transition-colors duration-200',
+		'flex h-10 items-center justify-center gap-2 rounded-lg border-3 px-4 py-2 font-semibold ring-offset-2 transition-colors duration-300',
 		'focus:ring-2 focus:outline-none',
-		'active:scale-98',
+		'active:scale-95 active:transition-transform',
 		'disabled:cursor-not-allowed disabled:opacity-50',
 		{
 			'border-emerald-600 bg-emerald-500 text-white hover:border-emerald-700 hover:bg-emerald-600 focus:ring-emerald-500 active:bg-emerald-700':
